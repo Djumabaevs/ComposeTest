9f342f2c3091cf92ee4ddaad4bc76af2ba32d7de
Djumabaevs <djumabaevb@gmail.com>
2021-06-15 11:04:14 +0600

    use card like card view in xml

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -7,15 +7,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composetest.ui.theme.ComposeTestTheme
@@ -49,6 +48,28 @@ class MainActivity : ComponentActivity() {
                 }
         }
     }
+
+
+    @Composable
+    fun ImageCard(
+        painter: Painter,
+        contentDescription: String,
+        title: String,
+         modifier: Modifier = Modifier) {
+
+        Card(
+            modifier = modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(15.dp),
+            elevation = 5.dp
+        ) {
+
+        }
+    }
+
+
+
+
+
 }
 /*
 
