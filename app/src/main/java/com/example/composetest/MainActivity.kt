082a95aa30a95b3d51b5f46f43d08ff3529f687d
Djumabaevs <djumabaevb@gmail.com>
2021-06-15 10:24:34 +0600

    fix no such method drawRect

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -3,10 +3,17 @@ package com.example.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetest.ui.theme.ComposeTestTheme
 
@@ -14,23 +21,29 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeTestTheme {
-                
-              Text(text = "Bakyt")
-            }
+                Column(modifier = Modifier
+                    .background(Color.Cyan)
+                    .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround
+                   ) {
+                    Text("Bakyt is")
+                    Text("Developer")
+                }
         }
     }
 }
+/*
 
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
+*/
 
-@Preview(showBackground = true)
+/*@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTestTheme {
         Greeting("Android")
-    }
-}
+    }*/
