804bd3662c66eafadd9a55035582ed491039f74f
Djumabaevs <djumabaevb@gmail.com>
2021-06-15 10:44:39 +0600

    properties of modifier applied sequentially

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,7 +14,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composetest.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,11 +25,18 @@ class MainActivity : ComponentActivity() {
         setContent {
                 Column(modifier = Modifier
                     .background(Color.Cyan)
-                    .fillMaxSize(),
+                    .fillMaxHeight(0.5f)
+                    .width(500.dp)
+                    .padding(20.dp)
+                    .border(5.dp, Color.DarkGray)
+                    .padding(15.dp)
+                    .border(10.dp, Color.Red, shape = RectangleShape),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.SpaceAround
                    ) {
-                    Text("Bakyt is")
+                    Text("Bakyt is", modifier = Modifier
+                        .offset(50.dp, 20.dp))
+                    Spacer(modifier = Modifier.height(200.dp))
                     Text("Developer")
                 }
         }
