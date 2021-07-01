698ce38509428246d8ec91116bfc7577a5bd435a
Djumabaevs <djumabaevb@gmail.com>
2021-07-01 14:38:01 +0600

    GridView

--- app/src/main/java/com/example/composetest/FromBookCompose.kt
+++ app/src/main/java/com/example/composetest/FromBookCompose.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
@@ -36,6 +37,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import kotlin.math.ceil
 
 @Composable
 fun AlertDialogScreen() {
@@ -488,7 +490,51 @@ data class IconResource(val imageVector: ImageVector, val isVisible: Boolean)
 
 
 
+@Composable
+fun GridScreen() {
+    GridView(columnCount = 3)
 
+    BackButtonHandler {
+        JetFundamentalsRouter.navigateTo(Screen.Navigation)
+    }
+}
+
+@Composable
+fun GridView(columnCount: Int) {
+    val itemSize = items.size
+    val rowCount = ceil(itemSize.toFloat() / columnCount).toInt()
+    val gridItems = mutableListOf<List<IconResource>>()
+    var position = 0
+
+    for(i in 0 until rowCount) {
+        val rowItem = mutableListOf<IconResource>()
+        for (j in 0 until columnCount) {
+            if(position.inc() <= itemSize) {
+                rowItem.add(IconResource(items[position++], true))
+            }
+        }
+
+        val itemsToFill = columnCount - rowItem.size
+        for (j in 0 until itemsToFill) {
+            rowItem.add(IconResource(Icons.Filled.Delete, false))
+        }
+        gridItems.add(rowItem)
+    }
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+        items(gridItems) {items ->
+            RowItem(items)
+        }
+
+    }
+}
+
+@Composable
+fun RowItem(rowItems: List<IconResource>) {
+    Row {
+        for (element in rowItems)
+            GridIcon(element)
+    }
+}
 
 
 
